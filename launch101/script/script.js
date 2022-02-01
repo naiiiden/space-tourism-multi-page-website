b578958121335a93ea67c2d1909629cc8f848743
naiden <na4ko359@gmail.com>
2022-02-01 15:32:11 +0200

    technology page completed & responsive/finishing touches/deleted unused starter files

--- launch101/script/script.js
+++ launch101/script/script.js
@@ -7,3 +7,37 @@ document.getElementById("hamburger").addEventListener("click", () => {
         document.getElementById("menu-icon").setAttribute("src", "starter-code/assets/shared/icon-hamburger.svg");
     };
 });
+
+let launchSlideIndex = 1;
+launchShowSlides(launchSlideIndex);
+
+function launchPlusSlides(n) {
+    launchShowSlides(launchSlideIndex += n);
+}
+
+function launchCurrentSlide(n) {
+    launchShowSlides(launchSlideIndex = n);
+}
+
+function launchShowSlides (n) {
+    let launchSlides = document.getElementsByClassName("launchSlide");
+    let launchButtons = document.getElementsByClassName("launchButton");
+
+    if (n > launchSlides.length) {
+        launchSlideIndex = 1;
+    }
+
+    if (n < 1) {
+        launchSlideIndex = launchSlides.length;
+    }
+
+    for (let i = 0; i < launchSlides.length; i++) {
+        launchSlides[i].style.display = "none";
+    }
+
+    for (i = 0; i < launchButtons.length; i++) {
+        launchButtons[i].className = launchButtons[i].className.replace(" active", "");
+    }
+    launchSlides[launchSlideIndex-1].style.display = "flex";
+    launchButtons[launchSlideIndex-1].className += " active";
+}
