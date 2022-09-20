b405e1309328a40b5652230402ce95019b4e7e45
naiiiden <na4ko359@gmail.com>
2022-09-20 12:27:09 +0300

    crew.html change mobile button menu aria label on close/open

--- crew/script/script.js
+++ crew/script/script.js
@@ -3,8 +3,10 @@ document.getElementById("hamburger").addEventListener("click", () => {
 
     if (document.getElementById("menu-icon").getAttribute("src") === "starter-code/assets/shared/icon-hamburger.svg") {
         document.getElementById("menu-icon").setAttribute("src", "starter-code/assets/shared/icon-close.svg");
+        document.getElementById("hamburger").ariaLabel = "Close menu";
     } else {
         document.getElementById("menu-icon").setAttribute("src", "starter-code/assets/shared/icon-hamburger.svg");
+        document.getElementById("hamburger").ariaLabel = "Open menu";
     };
 });
 
