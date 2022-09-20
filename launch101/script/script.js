f0dc60a3caaa8ed05503de0563c81f554aa69322
naiiiden <na4ko359@gmail.com>
2022-09-20 13:35:59 +0300

    launch101.html mobile menu links unfocusable if menu not open

--- launch101/script/script.js
+++ launch101/script/script.js
@@ -5,10 +5,16 @@ document.getElementById("hamburger").addEventListener("click", () => {
         document.getElementById("menu-icon").setAttribute("src", "starter-code/assets/shared/icon-close.svg");
         document.getElementById("hamburger").ariaLabel = "Close menu";
         document.getElementById("hamburger").ariaExpanded = "true";
+        document.querySelectorAll(".nav-ul .link").forEach(link => {
+            link.tabIndex = 0;
+        });
     } else {
         document.getElementById("menu-icon").setAttribute("src", "starter-code/assets/shared/icon-hamburger.svg");
         document.getElementById("hamburger").ariaLabel = "Open menu";
         document.getElementById("hamburger").ariaExpanded = "false";
+        document.querySelectorAll(".nav-ul .link").forEach(link => {
+            link.tabIndex = -1;
+        });
     };
 });
 
