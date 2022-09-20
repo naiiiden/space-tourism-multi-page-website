b747265295e9d2796e92bd64ed19e019c9cf1e46
naiiiden <na4ko359@gmail.com>
2022-09-20 13:17:46 +0300

    crew.html mobile menu button add aria-expanded property

--- crew/script/script.js
+++ crew/script/script.js
@@ -4,9 +4,11 @@ document.getElementById("hamburger").addEventListener("click", () => {
     if (document.getElementById("menu-icon").getAttribute("src") === "starter-code/assets/shared/icon-hamburger.svg") {
         document.getElementById("menu-icon").setAttribute("src", "starter-code/assets/shared/icon-close.svg");
         document.getElementById("hamburger").ariaLabel = "Close menu";
+        document.getElementById("hamburger").ariaExpanded = "true";
     } else {
         document.getElementById("menu-icon").setAttribute("src", "starter-code/assets/shared/icon-hamburger.svg");
         document.getElementById("hamburger").ariaLabel = "Open menu";
+        document.getElementById("hamburger").ariaExpanded = "false";
     };
 });
 
